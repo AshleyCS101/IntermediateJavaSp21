4a5bdd5101ca7d34d9ce1172da727b0779178eb7
Ashley Luty <Ashley Luty@LAPTOP-GS8OU5DR.home>
2021-02-06 04:38:03 -0800

    - updated week 1

--- src/week1/MyClass.java
+++ src/week1/MyClass.java
@@ -1,10 +1,19 @@
 package week1;
 
+//Welcome to Intermediate Java Programming!
+
 public class MyClass {
-
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        warmUp();
 
+    }
+    
+    public static void warmUp() {
+        // What is the output of the following?
+        
+        int a = 3/2;
+        System.out.println("The output of a is: " + a);        
     }
 
 }
+
