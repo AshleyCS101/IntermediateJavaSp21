3d17a47bd113150b764a410de42acbd84748dd00
Ashley Luty <Ashley Luty@LAPTOP-GS8OU5DR.home>
2021-02-06 16:11:38 -0800

    - week 2 warmup

---
+++ src/week2/WeekTwo.java
@@ -0,0 +1,28 @@
+package week2;
+
+import java.util.Scanner;
+
+// Strings and char
+
+public class WeekTwo {
+    public static void main(String[] args) {
+        // fyi
+//        Scanner scan = new Scanner(System.in);
+//        if(scan.nextFloat() == 1.0f) {
+//            System.out.println("Yay");
+//        }
+        
+        // Warm Ups
+        modWarmUp(7);   // What is the output of this function call?
+    }
+    
+    public static void modWarmUp(int n) {
+        for(int i = 0; i < 40; i++) {
+            int r = i % n;
+            System.out.println(r);
+            
+            //System.out.println(i + " % " + n + " is " + r);
+        }
+    }
+
+}
