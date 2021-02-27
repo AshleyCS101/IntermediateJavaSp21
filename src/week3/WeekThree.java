cb4477779d64fb9f0df6847cad7cfa43c7dc02f3
Ashley Luty <Ashley Luty@LAPTOP-GS8OU5DR.home>
2021-02-27 10:51:52 -0800

    - wk 3 starter

--- src/week3/WeekThree.java
+++ src/week3/WeekThree.java
@@ -16,27 +16,53 @@ public class WeekThree {
         
         System.out.println(itemCostMessage);
         System.out.println(totalCostMessage);
+        
+        /*
+         * Homework Review
+         */
+        
+        // prints ordinal number (int n)
+        
 
         /*
          * char
          */
+
+        
+        
+        // casting       
+        
+        
+        // print alphabet
+        
+        
+        // string-primitive concatenation
+
         
         /*
          * Methods
-         */
+         */   
         
+        //scope
         // follow execution order
         
+        // calling methods in methods
+        
+
     }
     
+    // return the smaller value
 //    public static int min(int a, int b) {
 //        
 //    }
+    // prints an average of the numbers
 //    public static void printAverage(int a, int b) {
-//        
+//
 //    }
+    
+    // returns the average of the numbers
 //    public static double getAverage(int a, int b) {
-//        
+//
 //    }
 //    public static void printBirthdayMessage(String name, int age) {
 //        
